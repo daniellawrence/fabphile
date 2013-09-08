e6d18e942beaf111ee75958ece8e806199f9cfb8
Daniel Lawrence <dannyla@linux.com>
2013-09-08 20:01:49 +1000

    added setup.py for packaging

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Fabphile',
+      version='0.0.1',
+      description='Fabric helper files',
+      author='',
+      author_email='',
+      url='',
+      package_dir={'': 'src'}, 
+      packages=['fabphile'],
+     )
